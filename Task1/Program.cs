271a21ddc6a00a1505357c19ffe1cc645cde9bb8
nikotsim <tsimbalyuk88@gmail.com>
2022-11-29 11:54:57 +0200

    changed first three strings

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Row row row");
+Console.WriteLine("Your boat");
+Console.WriteLine("Gently down the stream");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
